40d078f7bbbd6a0baf3ce588d181c0bc83d9dbd1
yulunyue <1285411603@qq.com>
2024-08-08 23:04:54 +0800

    c

--- common/algo/graph.py
+++ common/algo/graph.py
@@ -1,54 +1,76 @@
 from collections import defaultdict
+import heapq
 
 
 class Graph:
     def __init__(self) -> None:
         pass
 
-    def load_from_g(self,g):
-        self.g=g
-        self.keys=list(range(len(g)))
+    def load_from_g(self, g):
+        self.g = g
+        self.keys = list(range(len(g)))
         return self
-    
+
     def load_from_edge(self, edges):
-        self.g=defaultdict(lambda :defaultdict(int))
-        self.keys=[]
-        for f,t in edges:
+        self.g = defaultdict(lambda: defaultdict(int))
+        self.keys = []
+        for f, t in edges:
             self.keys.append(f)
             self.keys.append(t)
-            self.g[f][t]=1
-            self.g[t][f]=1
-        self.keys=list(set(self.keys))        
+            self.g[f][t] = 1
+            self.g[t][f] = 1
+        self.keys = list(set(self.keys))
         return self
-    
-    def log(self,*args):
+
+    def log(self, *args):
         pass
-    
-    def tarjan(self,b,init_ct=0):
-        low=defaultdict(lambda: init_ct)
-        vt=defaultdict(lambda: init_ct)
-        self.ct=init_ct
-        points=dict()
-        edges=[]
-        def dfs(n,p):
+
+    def tarjan(self, b, init_ct=0):
+        low = defaultdict(lambda: init_ct)
+        vt = defaultdict(lambda: init_ct)
+        self.ct = init_ct
+        points = dict()
+        edges = []
+
+        def dfs(n, p):
             # self.log(p,n)
-            self.ct+=1
-            vt[n]=low[n]=self.ct
-            c=0
+            self.ct += 1
+            vt[n] = low[n] = self.ct
+            c = 0
             for nv in self.g[n]:
-                if p==nv:
+                if p == nv:
                     continue
-                if vt[nv]==init_ct:
-                    c+=1
-                    dfs(nv,n)
-                    low[n]=min(low[nv],low[n])
-                    if vt[n]<=low[nv] and p !=-1:
-                        points[n]=True
-                    if vt[n]<low[nv]:
-                        edges.append([n,nv])
+                if vt[nv] == init_ct:
+                    c += 1
+                    dfs(nv, n)
+                    low[n] = min(low[nv], low[n])
+                    if vt[n] <= low[nv] and p != -1:
+                        points[n] = True
+                    if vt[n] < low[nv]:
+                        edges.append([n, nv])
                 else:
-                    low[n]=min(low[n],vt[nv])
-            if c>=2 and p == -1:
-                points[n]=True
-        dfs(b,-1)
-        return edges,points,low
+                    low[n] = min(low[n], vt[nv])
+            if c >= 2 and p == -1:
+                points[n] = True
+        dfs(b, -1)
+        return edges, points, low
+
+    def dijkstra(self, graph, start, n):
+        dist = [float('inf')] * n
+        dist[start] = 0
+        used = set()
+        q = [(0, start)]
+
+        while q:
+            u = heapq.heappop(q)[1]
+            if u in used:
+                continue
+
+            used.add(u)
+            for v, weight in graph[u].items():
+                target = dist[u] + weight
+                if target < dist[v]:
+                    dist[v] = target
+                    heapq.heappush(q, (dist[v], v))
+
+        return dist
