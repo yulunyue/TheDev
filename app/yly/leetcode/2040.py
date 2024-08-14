f8bd6b250b0c2510571904f2040c397f447d7787
y30033647 <yulunyue@huawei.com>
2024-08-14 17:29:45 +0800

    c

--- app/yly/leetcode/2040.py
+++ app/yly/leetcode/2040.py
@@ -24,17 +24,18 @@ class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
         l1, l2 = bisect.bisect_left(nums1, 0), bisect.bisect_right(nums1, 0)
         r1, r2 = bisect.bisect_left(nums2, 0), bisect.bisect_right(nums2, 0)
-        nums11, nums12 = nums1[:l1], nums1[l2:]
-        nums21, nums22 = nums2[:r1], nums2[r2:]
+        nums11, nums12 = [-v for v in nums1[:l1]], nums1[l2:]
+        nums21, nums22 = [-v for v in nums2[:r1]], nums2[r2:]
         small_zero_num = len(nums11)*len(nums22)+len(nums12)*len(nums21)
 
-        def uitl(array1, array2, array3, array4):
-            self.log(array1, array2, array3, array4)
+        def uitl(array1, array2, array3, array4, n):
+            self.log(array1, array2, array3, array4, n)
+            return 0
         if k <= small_zero_num:
-            return uitl(nums11, nums22, nums21, nums12)
+            return -uitl(nums11, nums22, nums21, nums12, small_zero_num-k)
         elif k <= small_zero_num+(l2-l1)*(r2-r1):
             return 0
-        return uitl(nums11, nums21, nums12, nums22)
+        return uitl(nums11, nums21, nums12, nums22, k)
 
     def test(self, **kg):
         return self.kthSmallestProduct(**kg)
