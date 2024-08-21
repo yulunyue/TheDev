3a2cce0b1aa930bfc1972dffbc0cdf7cda9e7054
yulunyue <1285411603@qq.com>
2024-08-21 23:05:57 +0800

    c

--- app/yly/leetcode/2234.py
+++ app/yly/leetcode/2234.py
@@ -18,6 +18,9 @@ class Solution:
     def get_cases(self):
         return [
             dict(
+                flowers=[5, 5, 15, 1, 9], newFlowers=36, target=12, full=9, partial=2, result=58,
+            ),
+            dict(
                 flowers=[20, 1, 15, 17, 10, 2, 4, 16, 15, 11],
                 newFlowers=2,
                 target=20,
@@ -46,30 +49,28 @@ class Solution:
         partial_need = [0]
         ret = flowers[0]*partial
         for i in range(n):
+            flowers[i] = min(flowers[i], target)
             partial_need.append(partial_need[-1]+flowers[i])
         if n*target <= newFlowers+partial_need[-1]:
             ret = max(ret, n*full)
-        self.log(flowers)
-        self.log(partial_need)
-        self.log(ret)
+        partial_num = target-1
+        # self.log(flowers)
+        # self.log(partial_need)
+        # self.log(ret)
         for i in range(n, 0, -1):
             full_has = partial_need[n]-partial_need[i]
             full_need = target*(n-i)-full_has
-            if full_need > newFlowers:
-                continue
+            if full_need < newFlowers:
+                break
             rest = newFlowers-full_need
-            l, r = flowers[0], target-1
-            partial_num = 0
-            while l <= r:
-                m = (l+r)//2
-                j = bisect.bisect_left(flowers, m)
-                if m*j-partial_need[j] <= rest:
-                    partial_num = m
-                    l = m+1
-                else:
-                    r = m-1
-            ret = max(ret, full_need, (n-i)*full+partial_num*partial)
-            self.log(i, flowers[i-1], n-i, partial_num, ret)
+            # partial_num = min(partial_num, flowers[i-1])
+            j = i
+            while partial_num*j-partial_need[j] > rest:
+                partial_num -= 1
+                j = bisect.bisect_left(flowers[:i], partial_num)
+
+            ret = max(ret, (n-i)*full+partial_num*partial)
+            self.log(i, flowers[i-1], n-i, partial_num, ret, rest)
         return ret
 
     def test(self, **kg):
