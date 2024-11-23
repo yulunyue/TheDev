aa3a92437881846664d3e494a7d6ee4fd0dddfc7
yly <1285411603@qq.com>
2024-11-24 01:09:11 +0800

    c

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from common.service.http import run, MainHander
 from app.yly.algo import manage
 from common.tool.ts_file import TsFile
-TS_MOCK_PATH = 'font/src/model/mock.ts'
+
 if __name__ == '__main__':
+    #TS_MOCK_PATH = 'font/src/model/mock.ts'
     #MainHander.POST_API.add_hock(TsFile().load(TS_MOCK_PATH).update_api)
     run(manage)
+
