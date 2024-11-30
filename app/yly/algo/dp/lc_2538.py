efd5c91698e88a38e0ce5043b8089bf4a681d15f
y30033647 <yulunyue@huawei.com>
2024-12-01 00:37:05 +0800

    c

--- app/yly/algo/dp/lc_2538.py
+++ app/yly/algo/dp/lc_2538.py
@@ -26,45 +26,43 @@ M = 10**9 + 7
 class Solution(SolutionBase):
     def get_cases(self):
         return [
+            dict(n =4,edges =[[2,0],[0,1],[1,3]],price =[2,3,1,1],result=6),
             dict(n=6, edges=[[0, 1], [1, 2], [1, 3], [3, 4], [
-                 3, 5]], price=[9, 8, 7, 6, 10, 5], result=24.1),
+                 3, 5]], price=[9, 8, 7, 6, 10, 5], result=24),
         ]
-
-    def execute(self, n: int, edges: List[List[int]], price: List[int]) -> int:
-        g = [[] for _ in range(n)]
+    def init(self,n: int, edges: List[List[int]], price: List[int]):
+        self.n=n
+        self.g = [[] for _ in range(n)]
+        self.price = price
         for x, y in edges:
-            g[x].append(y)
-            g[y].append(x)  # 建树
-        ans = 0
+            self.g[x].append(y)
+            self.g[y].append(x)  # 建树
 
-        def dfs(x: int, fa: int):
-
-            nonlocal ans
-
-            max_s1 = p = price[x]
-
-            max_s2 = 0
-
-            for y in g[x]:
-
-                if y == fa:
+    def execute(self,*args,**kg):
+        def dfs(c,p):
+            max_c2=0
+            for n in self.g[c]:
+                if n==p:
                     continue
+                max_c2=max(dfs(n,c),max_c2)
+            return max_c2+self.price[c]
+        return max(dfs(i,-1)-self.price[i] for i in range(self.n))
+    
+    def execute1(self,*args,**kg) -> int:
+        self.ans = 0
+        def dfs(c,p):
+            max_c1=self.price[c]
+            max_c2=0
+            for n in g[c]:
+                if n==p:
+                    continue
+                max_n1,maxn2=dfs(n,c)
+            return max_c1,max_c2            
 
-                s1, s2 = dfs(y, x)
-
-                ans = max(ans, max_s1 + s2, max_s2 + s1)
-
-                max_s1 = max(max_s1, s1 + p)
-
-                max_s2 = max(max_s2, s2 + p)  # 这里加上 p 是因为 x 必然不是叶子
-            self.log(fa, x, max_s1, max_s2, ans)
-            return max_s1, max_s2
-
-        dfs(0, -1)
-
-        return ans
-
+        return self.ans
+ 
     def maxOutput(self, *args, **kg):
+        self.init(*args,**kg)
         return self.execute(*args, **kg)
 
 
