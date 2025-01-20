6d591499e401af210ce0c25fc893402033a62fef
yly <1285411603@qq.com>
2025-01-20 23:23:34 +0800

    c

--- app/yly/algo/todo/cf_81e.py
+++ app/yly/algo/todo/cf_81e.py
@@ -22,23 +22,59 @@ class Solution(SolutionBase):
     uri='https://www.luogu.com.cn/problem/CF81E'
     def get_cases(self):
         return [
-
+            dict(input='''5
+5 2
+3 2
+5 1
+2 1
+4 2''',result='''2 2
+5 3
+4 2''')
         ]
     
-    def calc(self,u):
+    def calc(self,u,rt):
         self.f[u][0] = self.f[u][1] = Node(0,0)
+        for v in self.c[u]:
+            if v==rt:
+                continue
+            self.calc(v,rt)
+            self.f[u][0]=self.f[u][0].add(self.f[v][1])
+            t=self.f[v][0].sub(self.f[v][1]).add(Node(1,self.s[u]^self.s[v]))
+            if self.f[u][1].lt(t):
+                self.f[u][1]=t
+                self.g[u]=v
+        self.f[u][1]=self.f[u][0].add(self.f[u][0])
+    def get(self,u,i,rt):
+        for p in self.c[u]:
+            if p==rt:
+                continue
+            if not i or self.g[u] != p:
+                self.get(p,1,rt)
+            else:
+                self.p.append(Node(u,p))
+                self.get(p,0,rt)
+
     def solve(self,u):
         while not self.v[u]:
             self.v[u]=1
             u=self.in_id[u]
         r = Node(0,0)
-        for i in range(2):
-            self.rt=u
-            self.calc(u)
+        for _ in range(2):
+            self.calc(u,u)
+            if r.lt(self.f[u][1]):
+                r=self.f[u][1]
+                self.p.clear()
+                self.get(u,1,u)
+            u=self.in_id[u]
+        for p in self.p:
+            self.q.append(p)
+        self.ans=self.ans.add(r)
+
 
 
     def exec(self):
         self.n=self.i1()
+        self.f:List[List[Node]]=[[Node(0,0),Node(0,0)] for _ in range(self.n)]
         self.in_id=[None]*self.n
         self.g=[0]*self.n
         self.rt=0
