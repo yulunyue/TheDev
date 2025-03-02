cbf908cd0e5eb56d22797105aa2a5a20715897e0
yly <1285411603@qq.com>
2025-03-03 02:30:53 +0800

    c

--- app/yly/algo/todo/lc306.py
+++ app/yly/algo/todo/lc306.py
@@ -10,25 +10,18 @@ class Solution(SolutionBase):
         n=len(num)
         @functools.lru_cache(None)
         def dfs1(i1,i2):
-            c=0
-            k=i2-i1
-            i0=i1-k
-            
-            for m in range(k):
-                a2,a3=num[i2-m],num[i1-m]-c
-                if a3>a2:
-                    a1=a3-a2
-                    c=0
-                else:
-                    a1=a3+10-a2
-                    c=1
-           
-                return False
+            i0=2*i1-i2
+            last_c=0
+            for i in range(i2-i1):
+                a,b,c=num[i0-i],num[i1-i],num[i2-i]
+                if (a+b)%10==last_c+c:
+                    pass 
+        
             
                 
         @functools.lru_cache(None)
         def dfs0(j):
-            for i in range(j+1):
+            for i in range(j):
                 if dfs1(i,j):
                     return True
             return False
