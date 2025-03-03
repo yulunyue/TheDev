29c55354380207aaf620359324697db291438dd7
yly <1285411603@qq.com>
2025-03-03 23:47:41 +0800

    c

--- app/yly/algo/todo/lc306.py
+++ app/yly/algo/todo/lc306.py
@@ -13,16 +13,19 @@ class Solution(SolutionBase):
             i0=2*i1-i2
             last_c=0
             for i in range(i2-i1):
+                if i0-i<0:
+                    return False
                 a,b,c=num[i0-i],num[i1-i],num[i2-i]
-                if (a+b)%10==last_c+c:
-                    pass 
+                if (a+b)%10!=last_c+c:
+                    return False
+                last_c=1 if a+b>last_c+c else 0
+            return True
         
-            
-                
+    
         @functools.lru_cache(None)
         def dfs0(j):
             for i in range(j):
-                if dfs1(i,j):
+                if dfs1(i,j) and dfs0():
                     return True
             return False
         return dfs0(n-1)
