6b1b946659a7bcfd228801a8af84baadf41dc0e8
yly <1285411603@qq.com>
2025-05-09 00:58:46 +0800

    c

--- app/yly/algo/learn/bandit.py
+++ app/yly/algo/learn/bandit.py
@@ -1,40 +1,47 @@
-from common.algo.learn.env import Env
-from common.algo.learn.bernoulli import EpsilonGreedy,Algo,DecayingEpsilonGreedy,Ucb,ThompsonSampling
+from common.algo.learn.bernoulli import (
+    EpsilonGreedy,
+    Algo,
+    DecayingEpsilonGreedy,
+    Ucb,
+    ThompsonSampling,
+)
 from common.algo.manage import SolutionBase
 from typing import List
 import numpy as np
+
+
 class Bandit(Env):
-    K=10
+    K = 10
+
     def __init__(self):
-        self.probs=np.random.uniform(size=self.K)
-        self.max_idx=self.probs.argmax()
-        self.actions=list(range(self.K))
-     
-    
+        self.probs = np.random.uniform(size=self.K)
+        self.max_idx = self.probs.argmax()
+        self.actions = list(range(self.K))
+
     def do(self, action):
-        return 1 if np.random.rand()<self.probs[action] else 0
-    
-    def get_regret(self,action):
-        return self.probs[self.max_idx]-self.probs[action]
-    
+        return 1 if np.random.rand() < self.probs[action] else 0
+
+    def get_regret(self, action):
+        return self.probs[self.max_idx] - self.probs[action]
+
     def __str__(self):
-        return ",".join(["%.3f"%float(v) for v in self.probs])
-    
+        return ",".join(["%.3f" % float(v) for v in self.probs])
+
+
 class Solution(SolutionBase):
     def get_cases(self):
         return [
-            dict(tp='ep',epsilon=0.01),
-            dict(tp='de',epsilon=1),
-            dict(tp='ucb'),
-            dict(tp='ts')
+            dict(tp="ep", epsilon=0.01),
+            dict(tp="de", epsilon=1),
+            dict(tp="ucb"),
+            dict(tp="ts"),
         ]
-    
+
     def load(self):
         self.env = Bandit()
-        self.log(self.env)
-    
-    def execute(self,tp,**kw):
-        actor:Algo=dict(
+
+    def execute(self, tp, **kw):
+        actor: Algo = dict(
             ep=EpsilonGreedy,
             de=DecayingEpsilonGreedy,
             ucb=Ucb,
@@ -42,17 +49,17 @@ class Solution(SolutionBase):
         )[tp]()
         actor.set_env(self.env).load(**kw)
         ret = actor.run()
-        self.log(actor,actor.rewards_record[-1])
-        return ret 
-    
-    def run_finish(self,results:List[EpsilonGreedy]):
+        self.log(actor, actor.rewards_record[-1])
+        return ret
+
+    def run_finish(self, results: List[EpsilonGreedy]):
         from common.third_util.draw import Draw
-        Draw().draw_line([[
-            r.regrets_record,None,f'{r.__class__.__name__}'
-        ] for r in results]).save(f"data/log/{self.__class__.__name__}.jpg")
+
+        Draw().draw_line(
+            [[r.regrets_record, None, f"{r.__class__.__name__}"] for r in results]
+        ).save(f"data/log/{self.__class__.__name__}.jpg")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     np.random.seed(3)
     Solution().run()
-        
---
+++ common/algo/learn/test.py
@@ -0,0 +1,3 @@
+"""
+https://hrl.boyuai.com/chapter/1/%E5%88%9D%E6%8E%A2%E5%BC%BA%E5%8C%96%E5%AD%A6%E4%B9%A0
+"""
