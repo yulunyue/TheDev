cbe5e8625a98568d7a670f8a4e741c72f7b89474
yly <1285411603@qq.com>
2025-10-23 01:07:19 +0800

    c

---
+++ app/yly/algo/context/lc_3710.py
@@ -0,0 +1,8 @@
+from common.util.export import MockCf, List
+
+
+class Solution(MockCf):
+    def maxPartitionFactor(self, points: List[List[int]]) -> int:
+        pass
+
+    execute = maxPartitionFactor
---
+++ app/yly/algo/context/lc_3715.py
@@ -0,0 +1,6 @@
+from common.util.export import MockCf
+
+
+class Solution(MockCf):
+    def execute(self):
+        pass
