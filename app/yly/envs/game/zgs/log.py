1c1cef47641ad86a211f72631110ac762a77a856
y30033647 <yulunyue@huawei.com>
2025-10-23 22:34:13 +0800

    c

--- app/yly/envs/game/zgs/log.py
+++ app/yly/envs/game/zgs/log.py
@@ -2,10 +2,12 @@ from common.util.export import TheDevLoger
 
 
 class LogHelp:
-    logger: TheDevLoger
+    logger: TheDevLoger = None
     fz_num = 0
 
     def debug(self, msg):
+        if self.logger is None:
+            return
         self.logger.debug(msg)
 
 
