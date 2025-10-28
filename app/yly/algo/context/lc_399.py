01bba76a28f0bb153de4d05bf80cf627b6da7e45
yulunyue <1285411603@qq.com>
2025-10-28 11:05:14 +0800

    xx

---
+++ app/yly/algo/context/lc_399.py
@@ -0,0 +1,6 @@
+from common.util.export import Mockcf,List
+from common.algo.base
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+    execute=calcEquation
