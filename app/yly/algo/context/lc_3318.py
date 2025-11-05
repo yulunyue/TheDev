a553f0c3c6be38d54e3f0ee4e49a575c5d68722d
yulunyue <1285411603@qq.com>
2025-11-05 09:21:22 +0800

    xx

--- app/yly/algo/context/lc_3318.py
+++ app/yly/algo/context/lc_3318.py
@@ -1,23 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from common.util.export import MockCf,List,defaultdict
 from common.third_util.sort import SortedList
 class Solution(MockCf):
@@ -30,20 +10,25 @@ class Solution(MockCf):
         def rv(fl,v):
             if v in fl:
                 fl.remove(v)
+                return True
         def add(v):
             if ct[v]:
-                rv(sl,f(v))
+                if rv(sl,f(v)):
+                    self.s-=v*ct[v]
                 rv(sr,f(v))
             ct[v]+=1
-            if len(sr)<=x:
+            if len(sr)<x:
                 sr.add(f(v))
+                self.s+=v*ct[v]
             elif sr[0]<f(v):
+                rr=sr.pop(0)
                 sl.add(sr.pop(0))
                 sr.add(f(v))
+                self.s+=
 
         for i,v in enumerate(nums):
             add(v)
-            self.logger.map(v=v,sl=sl,sr=sr)
+            self.logger.map(v=v,sl=sl,sr=sr,s=self.s)
 
 
     execute=findXSum
