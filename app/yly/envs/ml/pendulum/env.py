7f8fa66cad5974cafc402438fae3536cff849c85
y30033647 <yulunyue@huawei.com>
2025-11-10 17:31:08 +0800

    c

--- app/yly/envs/ml/pendulum/env.py
+++ app/yly/envs/ml/pendulum/env.py
@@ -17,26 +17,19 @@ class PenduState(State):
     def __init__(self, state=None):
         if state is None:
             state, _ = self.env.reset()
-        self.done = False
         super().__init__(state)
 
-    def make_actions(self, depth=1, **kw):
-        if self.actions:
-            return self.actions
-        self.actions = dict()
+    def make_actions(self, **kw):
+        actions = []
         for i in range(C.n):
-            PenduState._env_ins.state = self.state
-            next_state, reward, terminated, _, _ = PenduState._env.step(
-                C.min + i * C.step
-            )
-            self.actions[i] = PenduState(
-                self, i, PenduState(next_state).set_done(terminated)
-            ).set_reward(reward)
-        return self.actions
+            next_state, reward, terminated, _, _ = self.env.step(C.min + i * C.step)
+            ns = PenduState(next_state).set_done(terminated)
+            actions.append(Action(self, i, ns).set_reward(reward))
+        return actions
 
     def to_str(self):
         return f"{self.state}"
 
-    # def reset(self):
-    #     self.state, _ = PenduState._env.reset()
-    #     return super().reset()
+    def reset(self):
+        self.state, _ = PenduState._env.reset()
+        return super().reset()
