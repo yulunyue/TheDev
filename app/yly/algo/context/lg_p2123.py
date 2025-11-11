e98e85bc1dcd61dbdd1fd32382f4975857ba825b
yly <1285411603@qq.com>
2025-11-12 00:11:29 +0800

    c

--- app/yly/algo/context/lg_p2123.py
+++ app/yly/algo/context/lg_p2123.py
@@ -4,9 +4,21 @@ from common.util.export import List, Dict, MockCf
 class Solution(MockCf):
     uri = "https://www.luogu.com.cn/problem/P2123"
 
+    def calc(self, a, b):
+        ans = 0
+        return ans
+
     def execute(self):
-        pass
+        ans = []
+        for _ in range(int(self.input())):
+            numsa, numsb = [], []
+            for _ in range(int(self.input())):
+                a, b = self.ii()
+                numsa.append(a)
+                numsb.append(b)
+            ans.append(self.calc(numsa, numsb))
+        return "\n".join([str(v) for v in ans])
 
 
 if __name__ == "__main__":
-    print(Solution().main())
+    print(Solution().execute())
