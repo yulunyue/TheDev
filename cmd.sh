9b9066ca88fafa9c8482b15e2580d37d31efbb28
yulunyue <1285411603@qq.com>
2025-11-21 14:12:22 +0800

    c

--- cmd.sh
+++ cmd.sh
@@ -2,5 +2,5 @@ python -m cpp.pytool.main execute cpp/src/loj_p2885.cpp
 python -m tests.test_algo
 python -m app.yly.main lc_757.py test
 python -m tests.test_tf
-python -m app.yly.zb.main vllm-project__vllm-3868 init
+python -m app.yly.zb.main vllm-project__vllm-3868 pytest
 python -m tool.pytes dev 
