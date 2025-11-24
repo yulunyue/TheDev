1f6b4478030ee2723bb227adaae99a5ce2f0e0ae
yly <1285411603@qq.com>
2025-11-24 23:55:37 +0800

    c

---
+++ app/yly/algo/context/lc_3003.py
@@ -0,0 +1,8 @@
+from common.util.export import List, MockCf
+
+
+class Solution(MockCf):
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+        pass
+
+    execute = maxPartitionsAfterOperations
