039974c04b22080b6a42758fffabf33f2fd659d0
yulunyue <1285411603@qq.com>
2025-12-11 09:02:15 +0800

    xx

--- app/yly/zb/manage.py
+++ app/yly/zb/manage.py
@@ -1,10 +1,18 @@
-from common.util.export import ToolBase
+from common.util.export import ToolBase,logger,File
 from .util import WT
 
 
 class ZbMangae(ToolBase):
     def query(self):
         WT.start()
+    def dev(self):
+        r=File("../../downloads")
+        for f in r.list_dir():
+            if not f.path.endswith(".zip"):
+                continue
+            name,pr=f.name.split("-")
+            owner,task_id,repo_name=name.split("_")
+            f.copy
 
 
 if __name__ == "__main__":
