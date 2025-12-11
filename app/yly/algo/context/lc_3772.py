4f3b26794455ce025abc09b301232c99814b8fa1
yulunyue <1285411603@qq.com>
2025-12-11 22:30:48 +0800

    c

--- app/yly/algo/context/lc_3772.py
+++ app/yly/algo/context/lc_3772.py
@@ -8,8 +8,11 @@ class Solution(MockCf):
           B       C     D
         / | \    / \   / \
        E  F  G  H   I  J  K
-
     
+    给定一颗n个节点的无向树，每个节点的值V要么是1，要么是-1
+    求每个节点的所有连通集的和Fm的最小值，  连通集为包含该节点的子树
+    定义子树的和为函数F
+    以A为根jid
     """
 
     def maxSubgraphScore(
