be294db4a42382834009348073e1fe8a21f6da48
yulunyue <1285411603@qq.com>
2025-12-11 17:29:17 +0800

    c

--- app/yly/zb/beeware__briefcase-1006/setup_repo.sh
+++
@@ -1,5 +0,0 @@
-set -e
-mkdir -p /testbed/beeware
-git config --global http.sslVerify false
-git clone https://github.com/beeware/briefcase.git /testbed/beeware/briefcase
-conda create -n testbed -y
