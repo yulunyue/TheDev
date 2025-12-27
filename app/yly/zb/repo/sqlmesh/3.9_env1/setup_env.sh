4569d0a2c28ac4a6638e545e7f7b11b093c6135a
yulunyue <1285411603@qq.com>
2025-12-27 20:06:31 +0800

    c

--- app/yly/zb/repo/sqlmesh/3.9_env1/setup_env.sh
+++ app/yly/zb/repo/sqlmesh/3.9_env1/setup_env.sh
@@ -1,3 +1,5 @@
+pip install -e examples/custom_materializations
+echo '__version__ = "0.0.0"' > sqlmesh/_version.py
 pip install 'PyGithub>=2.6.0'
 pip install "google-auth"
 pip install "google-cloud-bigquery"
--- app/yly/zb/repo/sqlmesh/4367/setup_env.sh
+++
@@ -1,3 +0,0 @@
-pip install "google-auth"
-pip install "google-cloud-bigquery"
-pip install "google-cloud-bigquery-storage"
