fae22140641db5ad6421fcf5c49ed121480d0377
yly <1285411603@qq.com>
2026-01-03 23:25:08 +0800

    c

--- common/algo/base/bin_util.py
+++ common/algo/base/bin_util.py
@@ -1,5 +1,5 @@
 from typing import List
-from common.util.export import logger
+from common.util.export import logger, defaultdict
 
 
 def p2(num=10**6, mod=None, extern=1):
@@ -61,3 +61,45 @@ def get_sub_bits(i) -> List[int]:
             break
         j = (j - 1) & i
     return ret
+
+
+def ss_or_dp(nums):
+    xor_all = 0
+    for v in nums:
+        xor_all |= v
+    f = [0] * (xor_all + 1)
+    for v in nums:
+        f[v] += 1
+    n = xor_all.bit_length()
+    for i in range(n):
+        u = 1 << i
+        v = 0
+        while v <= xor_all:
+            v = v | u
+            f[v] += f[v ^ u]
+            v += 1
+    return f
+
+
+def ss_or_dp(nums):
+    or_all = 0
+    for v in nums:
+        or_all |= v
+    w = or_all.bit_length()
+    u = 1 << w
+
+    f = [0] * u
+    for x in nums:
+        f[x] += 1
+    for i in range(w):
+        bit = 1 << i  # 避免在循环中反复计算 1 << i
+        if (
+            or_all & bit == 0
+        ):  # 优化：or_all 中是 0 但 s 中是 1 的 f[s] 后面容斥用不到，无需计算
+            continue
+        s = 0
+        while s < u:
+            s |= bit  # 快速跳到第 i 位是 1 的 s
+            f[s] += f[s ^ bit]
+            s += 1
+    return f
