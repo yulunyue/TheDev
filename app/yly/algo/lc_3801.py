0d8264b275ab804a64b21b0b478e8dd17c4ca41e
yulunyue <1285411603@qq.com>
2026-01-10 09:21:15 +0800

    xx

--- app/yly/algo/lc_3801.py
+++ app/yly/algo/lc_3801.py
@@ -3,4 +3,13 @@ class Solution:
         return dict(case0=dict(lists=[[1, 3, 5], [2, 4], [6, 7, 8]], result=18))
 
     def minMergeCost(self, lists: List[List[int]]) -> int:
-        pass
+        n=len(lists)
+        u=(1<<n)-1
+        sl=[[] for _ in range(u)]
+        for i in range(n):
+            m=1<<i
+            s[m]=lists[i]
+            while m:
+        def dfs(s):
+            
+
