de45fc33362e161fabc67f8702beb736ec8604fb
yly <1285411603@qq.com>
2026-01-24 00:11:33 +0800

    c

--- common/algo/base/pn_node.py
+++ common/algo/base/pn_node.py
@@ -4,9 +4,13 @@ from typing import List
 class PnNode:
     left: "PnNode" = None
     right: "PnNode" = None
+    is_remove = False
 
     def __init__(self, v):
         self.value = v
+
+    def get_value(self):
+        return self.value
 
     @classmethod
     def make(cls, array, func=None) -> List["PnNode"]:
@@ -14,7 +18,7 @@ class PnNode:
         for i, v in enumerate(array):
             if func is not None:
                 v = func(i, v)
-            n = PnNode(v)
+            n = cls(v)
             if i != 0:
                 n.set_left(ret[-1])
             ret.append(n)
@@ -35,7 +39,21 @@ class PnNode:
             self.left.set_right(self.right)
         elif self.right:
             self.right.set_left(self.left)
+        self.is_remove = True
         return self
 
+    def replace(self, node):
+        if self.left:
+            self.left.set_right(node)
+        if self.right:
+            self.right.set_left(node)
+        self.is_remove = True
+
+    def get_tail(self):
+        ret = self
+        while ret.left:
+            ret = ret.left
+        return ret
+
     def __lt__(self, p: "PnNode"):
-        return self.value < p.value
+        return self.get_value() < p.get_value()
