e1bb9332eaad24ee73ceef92082fcd9e60c02e60
yulunyue <1285411603@qq.com>
2026-01-26 10:17:19 +0800

    c

--- common/algo/base/comb.py
+++ common/algo/base/comb.py
@@ -2,7 +2,7 @@ from common.util.export import CT
 
 
 class Comb:
-    def load(self, mx=CT.MX, mod=CT.MOD):
+    def load(self, mx=CT.MX, mod=None):
         """(a//fac[i])%MOD == (a*self.inv_fac[i])%MOD"""
         self.mod = mod
         self.mx = mx
@@ -10,31 +10,25 @@ class Comb:
         self.fac = [0] * mx
         self.fac[0] = 1
         for i in range(1, mx):
-            self.fac[i] = (self.fac[i - 1] * i) % mod
+            self.fac[i] = self.calc_mod(self.fac[i - 1] * i)
 
         self.inv_fac = [0] * mx
-        self.inv_fac[mx - 1] = pow(self.fac[mx - 1], -1, mod)
+        self.inv_fac[mx - 1] = self.pow(self.fac[mx - 1], -1)
         for i in range(mx - 1, 0, -1):
-            self.inv_fac[i - 1] = (self.inv_fac[i] * i) % mod
+            self.inv_fac[i - 1] = self.calc_mod(self.inv_fac[i] * i)
         return self
 
+    def pow(self, a, b):
+        if self.mod is not None:
+            return pow(a, b, slef.mod)
+        return pow(a, b)
+
+    def calc_mod(self, v):
+        if self.mod is None:
+            return v
+        return v % self.mod
+
     def comb(self, n: int, k: int) -> int:
-        return (
-            ((self.fac[n] * self.inv_fac[k]) % self.mod)
-            * self.inv_fac[n - k]
-            % self.mod
+        return self.calc_mod(
+            self.calc_mod(self.fac[n] * self.inv_fac[k]) * self.inv_fac[n - k]
         )
-
-    def make_split(self, array, num):
-        ans = []
-
-        def dfs(i, a, b):
-            if i == len(array):
-                if len(a) == num:
-                    ans.append([a[:], b[:]])
-                return
-            dfs(i + 1, a + [array[i]], b)
-            dfs(i + 1, a, b + [array[i]])
-
-        dfs(0, [], [])
-        return ans
