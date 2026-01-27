bc092cc80d23b651535ffdf2233d3ba5a1cf32e4
yulunyue <1285411603@qq.com>
2026-01-27 09:12:26 +0800

    xx

---
+++ app/yly/algo/lg_p3128.py
@@ -0,0 +1,24 @@
+https://www.luogu.com.cn/problem/P3128#submit
+"""5 10
+3 4
+1 5
+4 2
+5 4
+5 4
+5 4
+3 5
+4 3
+4 3
+1 3
+3 5
+5 4
+1 5
+3 4
+9
+"""
+class Solution(MockCf):
+    def main(self):
+        n,k=self.ii()
+        for _ in range(n):
+if __name__=="__main__":
+    Solution().main()
