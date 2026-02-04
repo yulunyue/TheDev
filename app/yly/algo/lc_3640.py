b9d2100de3c8de497281b1c943b2189d1c8868e7
yulunyue <1285411603@qq.com>
2026-02-04 17:33:45 +0800

    c

--- app/yly/algo/lc_3640.py
+++ app/yly/algo/lc_3640.py
@@ -1,5 +1,13 @@
-class Solution:
+from common.util.export import List, MockCf
+
+
+class Solution(MockCf):
     def get_cases(self):
-        return dict()case0=nums = [0,-2,-1,-3,0,2,-1],result=-4
+        return dict(
+            case0=dict(nums=[0, -2, -1, -3, 0, 2, -1], result=-4),
+        )
+
     def maxSumTrionic(self, nums: List[int]) -> int:
         pass
+
+    execute = maxSumTrionic
