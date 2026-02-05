197b6c2267b1ed846436c869f7ef495e729dd56d
yulunyue <1285411603@qq.com>
2026-02-05 15:25:07 +0800

    c

--- app/yly/algo/lc_3826.py
+++ app/yly/algo/lc_3826.py
@@ -4,12 +4,16 @@ from common.util.export import List, MockCf, functools, CT
 class Solution(MockCf):
     def get_cases(self):
         return dict(
-            case0=dict(nums=[5, 1, 2, 1], k=2, result=25),
+            case0=dict(nums=[1, 1, 1], k=3, result=3),
+            case1=dict(nums=[1, 1, 1], k=2, result=4),
+            case2=dict(nums=[5, 1, 2, 1], k=2, result=25),
         )
 
     def minPartitionScore(self, nums: List[int], k: int) -> int:
         n = len(nums)
         s = [0]
+        if n == k:
+            return sum([v * (v + 1) // 2 for v in nums])
         for v in nums:
             s.append(s[-1] + v)
 
@@ -19,7 +23,7 @@ class Solution(MockCf):
                 v = s[-1] - s[i]
                 return v * v
             ans = CT.inf
-            for j in range(i + 1, n - k):
+            for j in range(i + 1, n + 1 - k):
                 v = s[j] - s[i]
                 a = v * v + dfs(j, k - 1)
                 if a < ans:
