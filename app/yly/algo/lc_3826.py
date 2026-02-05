06e2d8106e4f56ba846e646a502b7dd0bb4d7da1
yulunyue <1285411603@qq.com>
2026-02-05 16:55:05 +0800

    c

--- app/yly/algo/lc_3826.py
+++ app/yly/algo/lc_3826.py
@@ -8,8 +8,7 @@ class Solution(MockCf):
             case1=dict(nums=[1, 1, 1], k=2, result=4),
             case2=dict(nums=[13, 8, 19], k=2, result=421),
             case3=dict(nums=[5, 1, 2, 1], k=2, result=25),
-            case4=dict(nums=[18, 16, 50], k=2, result=25),
-            case5=dict(nums=[17, 2, 43, 11, 44], k=4, result=0),
+            case4=dict(nums=[36, 39, 33], k=2, result=3294),
         )
 
     def minPartitionScore(self, nums: List[int], k: int) -> int:
@@ -18,7 +17,7 @@ class Solution(MockCf):
 
         for v in nums:
             s.append(s[-1] + v)
-        # self.logger.info(s)
+        self.logger.info(s)
 
         @functools.lru_cache(None)
         def dfs(i, k):
@@ -34,8 +33,11 @@ class Solution(MockCf):
             while u < c and j < n - k + 1:
                 j += 1
                 u = s[j] - s[i]
-            # self.logger.map(i=i, j=j, k=k, v=v, c=c)
+            self.logger.map(i=i, j=j, k=k, v=v, c=c)
             a = u * u + dfs(j, k - 1)
+            if u > c and j - 1 > i:
+                u = s[j - 1] - s[i]
+                a = min(a, u * u + dfs(j - 1, k - 1))
             return a
 
         return (dfs(0, k) + s[-1]) // 2
