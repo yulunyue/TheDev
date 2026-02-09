2c1d347b3abde6b6b42eb46cac8576c1b878fa1c
yulunyue <1285411603@qq.com>
2026-02-09 15:41:08 +0800

    c

--- app/yly/algo/lc_3738.py
+++ app/yly/algo/lc_3738.py
@@ -5,6 +5,7 @@ class Solution(MockCf):
     def get_cases(self):
         return dict(
             case0=dict(nums=[1, 2, 3, 1, 2], result=4),
+            case1=dict(nums=[2, 2, 2, 2, 2], result=5),
         )
 
     def longestSubarray(self, nums: List[int]) -> int:
@@ -13,19 +14,19 @@ class Solution(MockCf):
         l = 0
         mn = 0
         idx = []
-        for i in range(1, n):
-            if nums[i] < nums[i - 1]:
+        for i in range(1, n + 1):
+            if i == n or nums[i] < nums[i - 1]:
                 ct = i - l
                 lt[i - 1] = rt[l] = ct
                 mn = max(ct + 1, mn)
                 l = i
-                if i + 1 < n and nums[i] < nums[i + 1]:
+                if i + 1 < n and nums[i] <= nums[i + 1]:
                     idx.append(i)
         for i in idx:
             if nums[i - 1] <= nums[i + 1]:
                 mn = max(mn, lt[i - 1] + rt[i + 1] + 1)
             elif i - 2 > 0 and nums[i - 2] <= nums[i]:
                 mn = max(mn, lt[i - 1] + rt[i + 1] + 1)
-        return mn
+        return min(mn, n)
 
     execute = longestSubarray
