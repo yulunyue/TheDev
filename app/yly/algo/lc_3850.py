308982f8e9271c554c6d75d54249d35d0a474547
yulunyue <1285411603@qq.com>
2026-02-25 08:26:59 +0800

    xx

--- app/yly/algo/lc_3850.py
+++ app/yly/algo/lc_3850.py
@@ -4,7 +4,12 @@ from common.algo.base.comb import Comb
 MX = 20
 C = Comb().load(MX)
 F = defaultdict(int)
+G=[]
 for k in range(MX):
+    G.append([])
+    for m in range(k+1):
+        for n in range(k+1-m):
+            G[-1].append([m,n,n-m])
     for j in range(MX - k * 2):
         for i in range(k + j * 2, MX):
             F[i, k] += C.comb(i, j + k) * C.comb(i - j - k, j)
@@ -30,21 +35,25 @@ class Solution(MockCf):
                 else:
                     kt[v] += 1
             ct[v] += 1
-
+        def u(a,b,c):
+            return C.comb(a,b)*C.comb(a-b,c)
         if k != 1:
             return 0
         a2346 = 0
-        for a6 in range(ct[6] + 1):
-            for a4 in range(ct[4] + 1):
-                for
-            c6, r6, r2, r3 = a6, ct[6] - c6, ct[2], ct[3]
-            while c6 - r6 <= min(r2 - kt[2], r3 - kt[3]):
-                c6 += 1
-                for k6 in range(r6 + 1):
-                    k2, k3 = kt[2] - k6, kt[3] - k6
-                    if k2 < 0 or k3 < 0:
-                        break
-                r6 -= 1
+        for l6,r6,c6 in G[ct[6]]:
+            k3,k2,a=kt[3]-c6,kt[2]-c6,u(ct[6],l6,r6)
+            for l3,r3,c3 in G[ct[3]]:
+                k3-=c3
+                if k3<0:
+                    break
+                if k3>0:
+                    continue
+                a*=u(ct[3],l3,r3)
+                for l4,r4,c4 in G[ct[4]]:
+                    k2-=c4*2
+                    a*=u(ct[4],l4,r4)
+                    if k2<0:k2=-k2
+                    if k2<=ct[2]:a*=F[ct[2],k2]
 
         a1, a5 = 3 ** ct[1], F[ct[5], kt[5]]
         if a2346 == 0:
