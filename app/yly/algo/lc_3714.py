1a4dd83e6982305de8f3f77254074e04f39a9860
yulunyue <1285411603@qq.com>
2026-02-26 21:39:58 +0800

    c

--- app/yly/algo/lc_3714.py
+++ app/yly/algo/lc_3714.py
@@ -7,45 +7,48 @@ class Solution(MockCf):
             case0=dict(s="abbac", result=4),
             case1=dict(s="aba", result=2),
             case2=dict(s="aabcc", result=3),
+            case3=dict(s="accc", result=3),
         )
+
+    def get_key(self, suf, ct: dict):
+        min_ch, mask = "c", dict(a="", b="", c="")
+        ret = []
+        for j in range(len(suf) - 1, -1, -1):
+            min_ch = min(min_ch, suf[j])
+            d = ct.copy()
+            for ch in suf[j:]:
+                d[ch] -= ct[min_ch]
+            mask[suf[j]] = "*"
+            p = ""
+            for k in "abc":
+                p += f"{k}{mask[k]}{d[k]}"
+            ret.append(p)
+        return ret
 
     def longestBalanced(self, s: str) -> int:
         n = len(s)
         ct = dict(a=0, b=0, c=0)
-        mp = dict()
+        pos = dict()
         ans = 1
+        t = ""
+        sufs = [""] * n
+        for i in range(n - 1, -1, -1):
+            v = s[i]
+            t = t.replace(v, "") + v
+            sufs[i] = t
+        t = ""
+        self.logger.info(s)
         for i, v in enumerate(s):
+            for k in self.get_key(sufs[i], ct):
+                if k not in pos:
+                    pos[k] = i - 1
+                    self.logger.map(i=i - 1, t1=sufs[i], k=k)
             ct[v] += 1
-            min_ct, max_ct, key = inf, -inf, "c"
-            nm = 0
-            for k, u in ct.items():
-                # if u == 0:
-                #     continue
-                if u < min_ct:
-                    key, min_ct = k, u
-                if u == min_ct:
-                    key = min(k, key)
-                if u > max_ct:
-                    max_ct = u
-                if u:
-                    nm += 1
-            if nm * max_ct == i + 1:
-                ans = i + 1
-            for k in "abc":
-                # if ct[k] == 0:
-                #     continue
-                if ct[k] >= min_ct:
-                    key += f"{ct[k]-min_ct}"
-
-            if key in mp:
-                ans = max(ans, i - mp[key])
-            else:
-                mp[key] = i
-
-            # if max_ct == min_ct:
-            #     ans = i + 1
-            self.logger.map(i=i, key=key, mp=mp, min_ct=min_ct, max_ct=max_ct, nm=nm)
-
+            t = t.replace(v, "") + v
+            for k in self.get_key(t, ct):
+                if k in pos:
+                    ans = max(ans, i - pos[k])
+                    self.logger.map(i=i, t2=t, k=k)
         return ans
 
     execute = longestBalanced
