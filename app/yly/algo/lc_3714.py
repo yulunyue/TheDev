7d6eb35e8854be5aba1db1e2c67182e989ca559b
yulunyue <1285411603@qq.com>
2026-02-26 11:39:21 +0800

    c

--- app/yly/algo/lc_3714.py
+++ app/yly/algo/lc_3714.py
@@ -1,27 +1,39 @@
-from common.util.export import MockCf, defaultdict
+from common.util.export import MockCf, defaultdict, inf
 
 
 class Solution(MockCf):
     def get_cases(self):
-        return dict(case0=dict(s="abbac", result=4))
+        return dict(
+            case0=dict(s="abbac", result=4),
+            case1=dict(s="aba", result=2),
+        )
 
     def longestBalanced(self, s: str) -> int:
         n = len(s)
         ct = dict(a=0, b=0, c=0)
-        keys = "abc"
         mp = dict()
         ans = 1
         for i, v in enumerate(s):
             ct[v] += 1
-            min_ct = min(ct.values())
+
+            min_ct, max_ct = inf, -inf
+            for u in ct.values():
+                if u == 0:
+                    continue
+                min_ct, max_ct = min(min_ct, u), max(max_ct, u)
+            if min_ct == max_ct:
+                ans = i + 1
+                continue
             key = ""
-            for k in keys:
-                key += str(ct[k] - min_ct)
+            for k, v in ct.items():
+                if v > min_ct:
+                    key += f"{k}:{v-min_ct};"
+
             if key in mp:
                 ans = max(ans, i - mp[key] + 1)
             else:
                 mp[key] = i
-            # self.logger.map(i=0, key=key, mp=mp)
+            self.logger.map(i=i, key=key, mp=mp)
 
         return ans
 
