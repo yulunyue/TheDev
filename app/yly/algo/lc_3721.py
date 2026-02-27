c23c9d87f1a61551b1aa557afd66d09bdb1cd2eb
yulunyue <1285411603@qq.com>
2026-02-27 18:33:43 +0800

    xx

--- app/yly/algo/lc_3721.py
+++ app/yly/algo/lc_3721.py
@@ -9,8 +9,9 @@ class Bk(Block):
         self.logger: TheDevLoger = logger
 
     def update(self, l, r, v):
-        self.logger.map(l=l, r=r, v=v)
-        return super().update(l, r, v)
+        a=super().update(l, r, v)
+        self.logger.map(l=l, r=r, v=v,todo=self.todo,data=self.data)
+        return a
 
     def set_datas(self, i, l, r, v):
         self.pos[i] = dict()
@@ -21,8 +22,6 @@ class Bk(Block):
         return super().set_data(i, v)
 
     def find(self, r, t):
-        if t == 0:
-            return -1
         i = j = 0
         while j + self.n < r:
             i += 1
@@ -39,25 +38,27 @@ class Bk(Block):
 class Solution(MockCf):
     def get_cases(self):
         return dict(
-            # case0=dict(nums=[2, 5, 4, 3], result=4),
+            case2=dict(nums=[1,3,5],result=0),    
+            case0=dict(nums=[2, 5, 4, 3], result=4),
             case1=dict(nums=[3, 2, 2, 5, 4], result=5),
         )
 
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
-        bs, ans = Bk(n, self.logger), 0
+        bs, ans = Bk(n+1, self.logger), 0
         last_idx = dict()
         self.logger.info(nums)
         for i, v in enumerate(nums):
             u = 1 if v % 2 else -1
-            bs.update(i, n - 1, u)
-            if v in last_idx:
+            if v not in last_idx:
+                bs.update(i+1, n, u)
+            else:
                 bs.update(last_idx[v], i, -u)
-            t = bs.get(i)
-            j = bs.find(i - ans, t)
+            t = bs.get(i+1)
+            j = bs.find(i+1- ans, t)
             if j is not None:
-                ans = max(i - j, ans)
-            last_idx[v] = i
+                ans = max(i+1 - j, ans)
+            last_idx[v] = i+1
             self.logger.map(i=i, j=j, v=v, ans=ans, todo=bs.todo, data=bs.data)
         return ans
 
