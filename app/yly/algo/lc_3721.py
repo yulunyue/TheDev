58ac46a8b3b182514a62a2735a91847f93b29c84
yulunyue <1285411603@qq.com>
2026-02-27 17:32:03 +0800

    c

--- app/yly/algo/lc_3721.py
+++ app/yly/algo/lc_3721.py
@@ -1,11 +1,16 @@
-from common.util.export import MockCf, List
+from common.util.export import MockCf, List, TheDevLoger
 from common.algo.base.block import Block
 
 
 class Bk(Block):
-    def __init__(self, size):
+    def __init__(self, size, logger):
         super().__init__(size)
         self.pos = [dict() for _ in range(self.n)]
+        self.logger: TheDevLoger = logger
+
+    def update(self, l, r, v):
+        self.logger.map(l=l, r=r, v=v)
+        return super().update(l, r, v)
 
     def set_datas(self, i, l, r, v):
         self.pos[i] = dict()
@@ -22,7 +27,7 @@ class Bk(Block):
         while j + self.n < r:
             i += 1
             if t in self.pos[i]:
-                return self.pop[i][t]
+                return self.pos[i][t]
             j += self.n
         while j < r:
             if self.get(j) == t:
@@ -34,25 +39,26 @@ class Bk(Block):
 class Solution(MockCf):
     def get_cases(self):
         return dict(
-            case0=dict(nums=[2, 5, 4, 3], result=4),
+            # case0=dict(nums=[2, 5, 4, 3], result=4),
             case1=dict(nums=[3, 2, 2, 5, 4], result=5),
         )
 
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
-        bs, ans = Bk(n), 0
+        bs, ans = Bk(n, self.logger), 0
         last_idx = dict()
+        self.logger.info(nums)
         for i, v in enumerate(nums):
             u = 1 if v % 2 else -1
             bs.update(i, n - 1, u)
             if v in last_idx:
-                for j in range(last_idx[v], i):
-                    bs.update(j, i, -u)
+                bs.update(last_idx[v], i, -u)
             t = bs.get(i)
             j = bs.find(i - ans, t)
             if j is not None:
-                ans = i - j
+                ans = max(i - j, ans)
             last_idx[v] = i
+            self.logger.map(i=i, j=j, v=v, ans=ans, todo=bs.todo, data=bs.data)
         return ans
 
     execute = longestBalanced
--- doc/tool/todo.md
+++ doc/tool/todo.md
@@ -1,5 +1,5 @@
 ```lc1
-app.yly.algo.lc_3714a::Solution::run
+app.yly.algo.lc_3721::Solution::run
 ```
 
 ```lc2
@@ -9,7 +9,7 @@ app.yly.algo.lc_3738::Solution::run
 app.yly.algo.lg_p3128::Solution::run
 ```
 ```debug
-app.yly.algo.lc_3850::Solution::run case4
+app.yly.algo.lc_3721::Solution::run case1
 ```
 ```pg
 common.third_util.view.pygraphviz_util::PyGraphViz::save
