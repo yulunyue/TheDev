9337015533cfa27726e98fd2b184e7fc575697e4
yly <1285411603@qq.com>
2026-03-01 00:01:51 +0800

    c

--- app/yly/algo/lc_3721.py
+++ app/yly/algo/lc_3721.py
@@ -9,17 +9,17 @@ class Bk(Block):
         self.logger: TheDevLoger = logger
 
     def update(self, l, r, v):
-        a=super().update(l, r, v)
-        self.logger.map(l=l, r=r, v=v,todo=self.todo,data=self.data)
+        a = super().update(l, r, v)
+        self.logger.map(l=l, r=r, v=v, todo=self.todo, data=self.data)
         return a
 
     def set_datas(self, i, l, r, v):
         self.pos[i] = dict()
         return super().set_datas(i, l, r, v)
 
-    def set_data(self, i, v):
-        self.pos[v] = i
-        return super().set_data(i, v)
+    def set_data(self, idx, i, v):
+        self.pos[idx][v] = i
+        return super().set_data(idx, i, v)
 
     def find(self, r, t):
         i = j = 0
@@ -38,27 +38,28 @@ class Bk(Block):
 class Solution(MockCf):
     def get_cases(self):
         return dict(
-            case2=dict(nums=[1,3,5],result=0),    
+            case2=dict(nums=[1, 3, 5], result=0),
             case0=dict(nums=[2, 5, 4, 3], result=4),
             case1=dict(nums=[3, 2, 2, 5, 4], result=5),
+            case3=dict(nums=[0, 2, 1], result=2),
         )
 
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
-        bs, ans = Bk(n+1, self.logger), 0
+        bs, ans = Bk(n + 1, self.logger), 0
         last_idx = dict()
         self.logger.info(nums)
         for i, v in enumerate(nums):
             u = 1 if v % 2 else -1
             if v not in last_idx:
-                bs.update(i+1, n, u)
+                bs.update(i + 1, n, u)
             else:
                 bs.update(last_idx[v], i, -u)
-            t = bs.get(i+1)
-            j = bs.find(i+1- ans, t)
+            t = bs.get(i + 1)
+            j = bs.find(i + 1 - ans, t)
             if j is not None:
-                ans = max(i+1 - j, ans)
-            last_idx[v] = i+1
+                ans = max(i + 1 - j, ans)
+            last_idx[v] = i + 1
             self.logger.map(i=i, j=j, v=v, ans=ans, todo=bs.todo, data=bs.data)
         return ans
 
--- common/algo/base/block.py
+++ common/algo/base/block.py
@@ -4,7 +4,7 @@ from common.util.export import math
 class Block:
     def __init__(self, size):
         self.n = math.ceil(math.sqrt(size))
-        self.m = math.ceil(size/self.n)
+        self.m = math.ceil(size / self.n)
         self.size = size
         self.data = [0] * size
         self.todo = [0] * self.m
@@ -20,17 +20,18 @@ class Block:
         i = idx // self.n
         return i, i * self.n
 
-    def set_data(self, i, v):
+    def set_data(self, idx, i, v):
         self.data[i] = v
 
     def update_area(self, i, l, r, v):
-        if l%self.n==0 and (r==self.size-1 or r-l==self.n-1):
+        if l % self.n == 0 and (r == self.size - 1 or r - l == self.n - 1):
             self.do(i, v)
             return
-        self.set_datas(i,l,r,v)
-    def set_datas(self,i,l,r,v):
+        self.set_datas(i, l, r, v)
+
+    def set_datas(self, i, l, r, v):
         for j in range(l, r + 1):
-            self.set_data(j, self.data[j] + v)
+            self.set_data(i, j, self.data[j] + v)
         self.down(i)
 
     def do(self, i, v):
@@ -56,9 +57,9 @@ class Block:
             return
         for j in range(self.n):
             idx = j + i * self.n
-            if idx>=self.size:
+            if idx >= self.size:
                 break
-            self.set_data(idx, self.data[idx] + self.todo[i])
+            self.set_data(i, idx, self.data[idx] + self.todo[i])
         self.todo[i] = 0
 
     def update(self, l, r, v):
