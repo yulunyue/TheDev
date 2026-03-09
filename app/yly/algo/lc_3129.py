5e39f8beecbb88feafb096d9c5614b8225b55a43
yulunyue <1285411603@qq.com>
2026-03-09 22:18:31 +0800

    c

--- app/yly/algo/lc_3129.py
+++ app/yly/algo/lc_3129.py
@@ -1,4 +1,4 @@
-from common.util.export import List, Dict, MockCf, functools, CT
+from common.util.export import List, Dict, MockCf, functools, CT, defaultdict, math
 from common.algo.base.comb import Comb
 
 C = Comb().load(201, CT.MOD)
@@ -12,7 +12,7 @@ class Solution(MockCf):
             case0=dict(zero=1, one=3, limit=1, result=0),
         )
 
-    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+    def numberOfStableArrays_dp(self, zero: int, one: int, limit: int) -> int:
 
         @functools.lru_cache(None)
         def dfs(i, j, k):
@@ -32,6 +32,30 @@ class Solution(MockCf):
 
         e = dfs(zero, one, 0) + dfs(zero, one, 1)
         dfs.cache_clear()
-        return e
+        return e % CT.MOD
+
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        if zero > one:
+            zero, one = one, zero
+        f0 = [0] * (zero + 3)
+
+        def u(zero, i):
+            r = 0
+            for j in range(1, (zero - i) // limit + 1):
+                c = -1 if j % 2 else 1
+                c *= C.comb(i, j) * C.comb(zero - j * limit - 1, i - 1)
+                r = (r + c) % CT.MOD
+            return r
+
+        for i in range(math.ceil(zero / limit), zero + 1):
+            f0[i] = C.comb(zero - 1, i - 1)
+            f0[i] = (f0[i] + u(zero, i)) % CT.MOD
+        ans = 0
+        for i in range(math.ceil(one / limit), min(one, zero + 1) + 1):
+            f1 = C.comb(one - 1, i - 1)
+            f1 = f1 + u(one, i)
+            ans = (ans + (f0[i - 1] + f0[i] * 2 + f0[i + 1]) * f1) % CT.MOD
+
+        return ans
 
     execute = numberOfStableArrays
