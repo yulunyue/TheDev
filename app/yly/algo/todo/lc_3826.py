6e8c2ab85a45d6257ca3e4ea24c6c8ea9342180b
yulunyue <1285411603@qq.com>
2026-03-11 09:05:07 +0800

    xx

--- app/yly/algo/todo/lc_3826.py
+++ app/yly/algo/todo/lc_3826.py
@@ -4,6 +4,8 @@ from common.util.export import MockCf
 class Solution(MockCf):
     """
     将一个长度为为n的正整数数组A，恰好分成连续的k个子数组，如何求k个子数组和的平方的和的最小值
+    a[i][j]=min(a[t][j-1]+(p[i]-p[t])**2,j-1<=t<=i)
+        =p[i]**2+min(a[t][j-1]+p[t]**2 -2*p[i] *p[t])
     """
 
     def get_cases(self):
@@ -13,6 +15,17 @@ class Solution(MockCf):
         )
 
     def minPartitionScore(self, nums: List[int], k: int) -> int:
-        pass
+        n=len(nums)
+        p=[0]*(n+1)
+        for i,v in enumerate(nums):
+            p[i+1]=p[i]+v
+        for j in range(k):
+            q=S()
+            for i in range(n):
+                x=p[i+1]
+                y=p[i+1]**2
+                if j>0:
+                    y*
+
 
     execute = minPartitionScore
