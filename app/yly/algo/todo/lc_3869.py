4a283cbf4f2515531019034da6437b4c423f5f9a
yulunyue <1285411603@qq.com>
2026-03-16 22:55:27 +0800

    xx

---
+++ app/yly/algo/todo/lc_3869.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getl = 12340, r = 12341
+
+输出： 1    
+    def countFancy(self, l: int, r: int) -> int:
+        
---
+++ app/yly/algo/todo/lc_3873.py
@@ -0,0 +1,7 @@
+class Solution:
+    def get_casea
+    points = [[1,1],[1,2],[2,2]]
+
+输出： 4
+    def maxActivated(self, points: list[list[int]]) -> int:
+        
