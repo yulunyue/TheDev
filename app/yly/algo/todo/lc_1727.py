c21fb29fee8aa9274f8cd1711c252407d2ef45e2
yulunyue <1285411603@qq.com>
2026-03-17 21:00:28 +0800

    c

--- app/yly/algo/todo/lc_1727.py
+++ app/yly/algo/todo/lc_1727.py
@@ -4,18 +4,30 @@ from common.util.export import List, MockCf, CT
 class Solution(MockCf):
     def get_cases(self):
         return dict(
-            case0=dict(matrix=[[0, 0, 1], [1, 1, 1], [1, 0, 1]], result=4),
+            case0=dict(
+                matrix=[[0, 0, 1], [1, 1, 1], [1, 0, 1]],
+                result=4,
+            ),
+            case1=dict(
+                matrix=[[1, 1, 0], [1, 0, 1]],
+                result=2,
+            ),
         )
 
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
         n, m = len(matrix), len(matrix[0])
         a = 0
         h = [0] * m
+        ix = range(m)
         for i in range(n):
             idx = [[], []]
             for j in range(m):
-                h[j] = h[j] * matrix[i][j] + matrix[i][j]
-                idx[matrix[i][j]].append(j)
+                if matrix[i][j] == 0:
+                    h[j] = 0
+                else:
+                    h[j] += 1
+                idx[matrix[i][j]].append(ix[j])
+            ix = idx[0] + idx[1]
             for j, v in enumerate(idx[1]):
                 a = CT.max(a, (m - j - len(idx[0])) * h[v])
         return a
