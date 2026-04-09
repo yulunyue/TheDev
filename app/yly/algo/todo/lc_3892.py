26f823ea6b93bf5ea84f4ed020063598090d223e
yulunyue <1285411603@qq.com>
2026-04-09 08:40:08 +0800

    xx

---
+++ app/yly/algo/todo/lc_3892.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    给定一个
+    贪心策略
+    对于任一个高峰的三个点，我们增加它没有意义
+    
+    """
+    def minOperations(self, nums: list[int], k: int) -> int:
+        nums = [4,5,3,6], k = 2
+
+输出： 0
