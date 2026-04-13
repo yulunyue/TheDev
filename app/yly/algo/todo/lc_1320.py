fabfc799f1af1d522b318ad0d0ff01213a1fc7c6
yulunyue <1285411603@qq.com>
2026-04-13 08:20:32 +0800

    xx

--- app/yly/algo/todo/lc_1320.py
+++ app/yly/algo/todo/lc_1320.py
@@ -1,4 +1,24 @@
 class Solution:
     def minimumDistance(self, word: str) -> int:
         word = "HAPPY"
-输出：6
+        result=6
+        def fp(v):
+            v=ord(v)-ord("A")
+            return v//6,v%6
+        def d(a,b):
+            ay,ax=fp(a)
+            by,bx=fp(b)
+            return abs(ay-by)+abs(ax-bx)
+        @functions.lru_cache(None)
+        def dfs(i,la,lb):
+            if i==len(word):
+                return 0
+            lc=word[i]
+            v=d(la,lc)+dfs(i+1,lc,lb)
+            if lb is None:
+                u=dfs(i+1,la,lc)
+            else:
+                u=d(lb,lc)+dfs(i+1,la,lc)
+            return u if u<v else v
+        return dfs(1,word[0],None)
+
