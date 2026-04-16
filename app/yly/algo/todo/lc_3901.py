9a0c5d57716fef7bd07de598e01f102914f066a2
yulunyue <1285411603@qq.com>
2026-04-16 21:41:46 +0800

    c

--- app/yly/algo/todo/lc_3901.py
+++ app/yly/algo/todo/lc_3901.py
@@ -11,7 +11,10 @@ class Solution(MockCf):
 
     def get_cases(self):
         return dict(
-            case0=dict(nums=[4, 8, 12, 16], p=2, queries=[[0, 3], [2, 6]], result=1)
+            case0=dict(nums=[4, 8, 12, 16], p=2, queries=[[0, 3], [2, 6]], result=1),
+            case1=dict(
+                nums=[4, 5, 7, 8], p=3, queries=[[0, 6], [1, 9], [2, 3]], result=2
+            ),
         )
 
     def countGoodSubseq(self, nums: list[int], p: int, queries: list[list[int]]) -> int:
@@ -28,13 +31,15 @@ class Solution(MockCf):
 
         for v in nums:
             c(v, 1)
+        ans = 0
         self.log(n=self.n, ct=dict(ct))
-        for j, (i, v) in enumerate(queries):
+        for i, v in queries:
             c(v, 1)
             c(nums[i], -1)
             self.log(n=self.n, ct=dict(ct))
             nums[i] = v
             if max(ct.values()) < self.n:
-                return j
+                ans += 1
+        return ans
 
     execute = countGoodSubseq
