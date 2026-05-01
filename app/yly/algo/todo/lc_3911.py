5b5ca73f36935260a13c999a4d50090b80da00cd
yulunyu <you@example.com>
2026-05-01 16:59:41 +0800

    xx

--- app/yly/algo/todo/lc_3911.py
+++ app/yly/algo/todo/lc_3911.py
@@ -1,4 +1,7 @@
 class Solution:
+    """
+    给定一个长度为n的数组nums，
+    """
     def kthRemainingInteger(self, nums: list[int], queries: list[list[int]]) -> list[int]:
         nums = [1,4,7], queries = [[0,2,1],[1,1,2],[0,0,3]]
 
