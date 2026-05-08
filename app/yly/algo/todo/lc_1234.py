fec77c87d6804f3bfc807083391a44c7831a60bc
yulunyu <you@example.com>
2026-05-08 18:21:43 +0800

    xx

--- app/yly/algo/todo/lc_1234.py
+++ app/yly/algo/todo/lc_1234.py
@@ -3,8 +3,18 @@ from common.util.export import List, Dict, functools, CT
 
 class Solution:
     def balancedString(self, s: str) -> int:
-        ct = dict(Q=0, W=0, E=0, R=0)
-        n = len(s)
+        c = dict(Q=0, W=0, E=0, R=0)
+        ci,cj=c.copy(),c.copy()
         for v in s:
-            ct[v] += 1
-        return
+            c[v]+=1
+        n = len(s)
+        m=n//4
+        
+        j=0
+        ans=n
+        for i,v in enumerate(s):
+            while j<n and not check():
+                cj[j]+=1
+            ans=min(ans,j-i)
+            ci[v]+=1        
+        return ans
